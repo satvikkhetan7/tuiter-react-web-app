c52c4f8de3562cf0cedab6f5559a4435433ad23c
satvikkhetan7 <satvikkhetancode@gmail.com>
2022-11-21 01:22:45 +0530

    rearranging order

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /*import logo from './logo.svg';*/
 import './App.css';
-import Labs from "./labs";
+// import Labs from "./labs";
 import HelloWorld from "./labs/a6/hello-world";
 import {BrowserRouter} from "react-router-dom";
 import {Routes,Route} from "react-router";
