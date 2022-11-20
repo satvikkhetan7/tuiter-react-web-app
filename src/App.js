e3e515a6ce279a927f31b475c6a74435d17198b0
satvikkhetan7 <satvikkhetancode@gmail.com>
2022-11-21 01:31:33 +0530

    rearranging order

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /*import logo from './logo.svg';*/
 import './App.css';
-// import Labs from "./labs";
+import Labs from "./labs";
 import HelloWorld from "./labs/a6/hello-world";
 import {BrowserRouter} from "react-router-dom";
 import {Routes,Route} from "react-router";
@@ -20,7 +20,7 @@ function App() {
                   <Route path="/tuiter/explore" element={<Explore/>}/>
                   <Route path="/tuiter/profile" element={<ProfileComponent/>}/>
                   <Route path="/tuiter/edit-profile" element={<EditProfileComponent/>}/>
-                  {/*<Route path="/*" element={<Labs/>}/>*/}
+                  <Route path="/labs" element={<Labs/>}/>
               </Routes>
           </div>
       </BrowserRouter>
