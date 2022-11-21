ae41aff16c78c6ee1b52bfff6d53b6440c962f38
satvikkhetan7 <satvikkhetancode@gmail.com>
2022-11-21 09:39:59 +0530

    adding redirect file

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
                   <Route path="/tuiter/explore" element={<Explore/>}/>
                   <Route path="/tuiter/profile" element={<ProfileComponent/>}/>
                   <Route path="/tuiter/edit-profile" element={<EditProfileComponent/>}/>
-                  <Route path="/labs" element={<Labs/>}/>
+                  <Route path="/*" element={<Labs/>}/>
               </Routes>
           </div>
       </BrowserRouter>
